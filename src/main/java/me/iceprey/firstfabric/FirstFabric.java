004d76424cf5a6cf35dfedef5c3ed89c54b054cd
ICEPrey <40725969+ICEPrey@users.noreply.github.com>
2021-11-14 16:12:21 -0800

    Add Ruby

--- src/main/java/me/iceprey/firstfabric/FirstFabric.java
+++ src/main/java/me/iceprey/firstfabric/FirstFabric.java
@@ -1,13 +1,17 @@
 package me.iceprey.firstfabric;
 
+import me.iceprey.firstfabric.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class FirstFabric implements ModInitializer {
+	public static final String MOD_ID = "firstfabric";
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		ModItems.registerModItems();
 
 		System.out.println("Hello Fabric world!");
 	}
